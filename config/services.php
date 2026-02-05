a8f93fe47b90b66973867277ae450363f402c681
MIR-FAHIM <ridoyfahim92@gmail.com>
2026-02-05 14:49:28 +0600

    new

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
 
     'muthobarta' => [
         'base_url' => env('MUTHOBARTA_BASE_URL', 'https://sysadmin.muthobarta.com/api/v1'),
-        'api_key' => env('MUTHOBARTA_API_KEY','Token 59ab62e2286088aeefe0d0ccd35c9dc34af3179e' ),
+        'api_key' => env('MUTHOBARTA_API_KEY','Token e76076c94b87ad21028fd2c13f931f727c6e5e24' ),
     ],
 
 ];
